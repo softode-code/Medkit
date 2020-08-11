a004f6d5ff6d08e1239ec3587adb7ae5ef972d97
Softode <softode-code@users.noreply.github.com>
2020-08-11 16:38:07 +0500

    Login to Firebase

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -14,6 +14,17 @@ class AuthService {
     }
   }
 
+  Future login(String email, String password) async {
+    try{
+      AuthResult result = await _auth.signInWithEmailAndPassword(email: email, password: password);
+      FirebaseUser user = result.user;
+      return user;
+    } catch (e) {
+      print(e.toString());
+      return null;
+    }
+  }
+
   Future signOut() async{
     try{
       return _auth.signOut();
